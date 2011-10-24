395456648f7ae0b5d96b4ffbbef197e21b5146de
unknown <David@David-mbp-win7.(none)>
2011-10-24 22:04:02 +0200

    Forsta commit...

---
+++ KarateChop.Tests/KarateChopForLoopTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Sdk;
+
+namespace KarateChop.Tests
+{
+    public class KarateChopForLoopTests
+    {
+        private int[] intArray;
+
+        private static void Chop(int search, int[] intArray, int index)
+        {
+            Assert.Equal(index, Karate.Chop(search, intArray));
+        }
+
+        [Fact]
+        public void EmtyArray_Returns_minus1()
+        {
+            Assert.Equal(-1, Karate.Chop(1, new int[] { }));
+        }
+
+        [Fact]
+        public void NoHit_Returns_minus1()
+        {
+            Assert.Equal(-1, Karate.Chop(1, new int[] { 3 }));
+
+        }
+
+        [Fact]
+        public void Hit_Returns_Index()
+        {
+            intArray = new int[] { 1 };
+            Chop(1, intArray, 0);
+        }
+
+        [Fact]
+        public void Hit_Returns_Index_Three_Field_Array()
+        {
+            intArray = new int[] { 1, 3, 5 };
+            Chop(3, intArray, 1);
+        }
+
+    }
+    public class KarateChopTheRealChopTests
+    {
+        private int[] intArray;
+        /// <summary>
+        /// Initializes a new instance of the KarateChopTheRealChopTests class.
+        /// </summary>
+        
+        public KarateChopTheRealChopTests()
+        {
+            intArray = CreateArray(5000);
+        }
+
+        private int[] CreateArray(int positioner)
+        {
+            var numberlist = new List<int>();
+
+            for (int i = 0; i < positioner; i++)
+            {
+                if (i % 2 == 0)
+                    numberlist.Add(i);
+            }
+
+            numberlist.Sort();
+            return numberlist.ToArray();
+        }
+
+        [Fact]
+        public void Zero_returns_minus1()
+        {
+            Assert.Equal(-1, Karate.ChopRecursive(0, intArray));
+        }
+
+        [Fact]
+        public void Ten_returns_Index_four()
+        {
+            Assert.Equal(4, Karate.ChopRecursive(10, intArray));
+        }
+    }
+}
---
+++ KarateChop/Karate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KarateChop
+{
+    public static class Karate
+    {
+        public static int Chop(int searchValue, int[] intArray)
+        {
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i].Equals(searchValue))
+                    return i;
+            }
+            return -1;
+        }
+        public static int ChopRecursive(int value, int[] intArray)
+        {
+            var interval = GetInterval(value, intArray, 0, intArray.Length);
+
+
+            while (true)
+            {
+                if (intArray[interval.Item1] == value)
+                {
+                    return interval.Item1;
+                }
+
+                if (intArray[interval.Item2] == value)
+                {
+                    return interval.Item2;
+                }
+
+                if (interval.Item1 == interval.Item2)
+                {
+                    if (intArray[interval.Item1] == value)
+                    {
+                        return interval.Item1;
+                    }
+                    return -1;
+                }
+
+                interval = GetInterval(value, intArray, interval.Item1, interval.Item2);
+
+            }
+        }
+
+        private static Tuple<int, int> GetInterval(int value, int[] intArray, int startindex, int slutindex)
+        {
+            if (intArray[(slutindex - startindex) / 2] > value)
+                return new Tuple<int, int>(startindex, slutindex / 2);
+
+            return new Tuple<int, int>(startindex + ((slutindex - startindex) / 2), slutindex);
+        }
+
+
+
+    }
+}
